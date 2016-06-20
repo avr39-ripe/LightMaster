6e785f656fa6ab57b7abd8f5db8922f9c0d44bee
Alexander V. Ribchansky <avr39ripe@gmail.com>
2016-06-21 01:30:05 +0300

    First try to implement Web UI with websockets
    Overall working, but lacks hw switch state send to Web UI

--- app/app.cpp
+++ app/app.cpp
@@ -10,6 +10,10 @@
 
 //AppClass
 //void monitor(HttpRequest &request, HttpResponse &response); // Monitor via json some important params
+//void wsConnected(WebSocket& socket);
+//void wsMessageReceived(WebSocket& socket, const String& message);
+//void wsBinaryReceived(WebSocket& socket, uint8_t* data, size_t size);
+//void wsDisconnected(WebSocket& socket);
 
 void AppClass::init()
 {
@@ -54,8 +58,80 @@ void AppClass::init()
 #endif
 	ApplicationClass::init();
 	webServer.addPath("/button",HttpPathDelegate(&BinHttpButtonsClass::onHttp,httpButtons));
+	// Web Sockets configuration
+	webServer.enableWebSockets(true);
+	webServer.setWebSocketConnectionHandler(WebSocketDelegate(&AppClass::wsConnected,this));
+	webServer.setWebSocketMessageHandler(WebSocketMessageDelegate(&AppClass::wsMessageReceived,this));
+	webServer.setWebSocketBinaryHandler(WebSocketBinaryDelegate(&AppClass::wsBinaryReceived,this));
+	webServer.setWebSocketDisconnectionHandler(WebSocketDelegate(&AppClass::wsDisconnected,this));
 //	webServer.addPath("/monitor",monitor);
 //	Serial.printf("AppClass init done!\n");
+}
+
+//WebSocket handling
+void AppClass::wsConnected(WebSocket& socket)
+{
+	Serial.printf("Websocket CONNECTED!\n");
+	// Notify everybody about new connection
+//	WebSocketsList &clients = server.getActiveWebSockets();
+//	for (int i = 0; i < clients.count(); i++)
+//		clients[i].sendString("New friend arrived! Total: " + String(totalActiveSockets));
+}
+
+void AppClass::wsMessageReceived(WebSocket& socket, const String& message)
+{
+	Serial.printf("WebSocket message received:\r\n%s\r\n", message.c_str());
+	String msg = message;
+	DynamicJsonBuffer jsonBuffer;
+	JsonObject& root = jsonBuffer.parseObject(msg);
+	//Uncomment next line for extra debuginfo
+	root.prettyPrintTo(Serial);
+	String command = root["command"];
+	Serial.printf("Command str: %s\n", command.c_str());
+	if (command == "setButton")
+	{
+//		Serial.println("command == setButton");
+//	}
+		if (root["button"].success())
+		{
+			if (root["state"].success()) // There is loopInterval parameter in json
+			{
+				uint8_t button = root["button"];
+				uint8_t state = root["state"];
+				httpButtons->setButton(button,state);
+				//Create response json with output status
+//				httpButtons->wsSendButton(button,state);
+				JsonObject& respRoot = jsonBuffer.createObject();
+				respRoot["response"] = "getButton";
+				respRoot["button"] = button;
+				respRoot["state"] = httpButtons->getButton(button);
+				respRoot.prettyPrintTo(Serial);
+				String buf;
+				respRoot.printTo(buf);
+				WebSocketsList &clients = webServer.getActiveWebSockets();
+				for (int i = 0; i < clients.count(); i++)
+				{
+					clients[i].sendString(buf);
+				}
+			}
+		}
+	}
+//	String response = "Echo: " + message;
+//	socket.sendString(response);
+}
+
+void AppClass::wsBinaryReceived(WebSocket& socket, uint8_t* data, size_t size)
+{
+	Serial.printf("Websocket binary data recieved, size: %d\r\n", size);
+}
+
+void AppClass::wsDisconnected(WebSocket& socket)
+{
+	Serial.printf("Websocket DISCONNECTED!\n");
+	// Notify everybody about lost connection
+//	WebSocketsList &clients = server.getActiveWebSockets();
+//	for (int i = 0; i < clients.count(); i++)
+//		clients[i].sendString("We lost our friend :( Total: " + String(totalActiveSockets));
 }
 
 void AppClass::start()
--- include/app.h
+++ include/app.h
@@ -14,6 +14,10 @@ class AppClass : public ApplicationClass
 public:
 	virtual void init(); // Application initialization
 	virtual void start(); // Application main-loop start/restart
+	void wsConnected(WebSocket& socket);
+	void wsMessageReceived(WebSocket& socket, const String& message);
+	void wsBinaryReceived(WebSocket& socket, uint8_t* data, size_t size);
+	void wsDisconnected(WebSocket& socket);
 protected:
 	virtual void _loop(); // Application main loop function goes here
 };
