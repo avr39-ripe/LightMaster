28ebc06cab573fda77d1ce5ac3caf36157395572
Alexander V. Ribchansky <avr39ripe@gmail.com>
2016-07-27 18:04:14 +0300

    Some minor cleanup from comments and unused code

--- files/index.js
+++ files/index.js
@@ -1,24 +1,18 @@
 'use strict';
 
+var binStates;
 //Websockets
 var websocket;
+
 function onOpen(evt) {
 	console.log.bind(console)("CONNECTED");
+	
 	wsGetAppStatus();
 	setInterval(wsGetAppStatus, 5000);
 	
 	binStates = new BinStatesClass();
-	
 	setTimeout(function() { binStates.wsGetAllButtons(); }, 500)
 	setTimeout(function() { binStates.wsGetAllStates(); }, 850)
-	//binStates = new BinStatesClass();
-	// binState = new BinStateClass(0);
-	// binState.wsGetName();
-	// binState.wsGetState();
-}
-
-function onClose(evt) {
-	console.log.bind(console)("DISCONNECTED");
 }
 
 function onMessage(evt) {
@@ -34,7 +28,6 @@ function onMessage(evt) {
     	if ( cmd == wsBinConst.getResponse && sysId == 1 && subCmd == wsBinConst.scAppGetStatus ) {
     		var counter = bin.getUint32(wsBinConst.wsPayLoadStart, true);
     		var timestamp = bin.getUint32(wsBinConst.wsPayLoadStart + 4, true);
- //   		console.log.bind(console)(`counter = ${counter}, timestamp = ${timestamp}`);
     		
     		document.getElementById("counter").textContent = counter;
     		var d = new Date();
@@ -42,23 +35,15 @@ function onMessage(evt) {
     		document.getElementById("dateTime").textContent = d.toLocaleString();
     	}
     	
-    	// if ( cmd == wsBinConst.getResponse && sysId == 2 ) {
-    		// binState.wsBinProcess(bin);
-    	// }
     	if ( cmd == wsBinConst.getResponse && ( sysId == 2 || sysId == 3) ) {
     		binStates.wsBinProcess(bin);
     	}
     		
   	} 
-  	// else {
-    	// var json = JSON.parse(evt.data);
-		// console.log.bind(console)("Json recv: " + json);
-// 	
-		// if (json.response == "getAppState") {
-			// onGetAppState(json);
-		// }
-//  	}
+}
 
+function onClose(evt) {
+	console.log.bind(console)("DISCONNECTED");
 }
 
 function onError(evt) {
@@ -75,12 +60,7 @@ function initWS() {
 	websocket.binaryType = 'arraybuffer';
 }
 
-function closeWS() {
-	websocket.close();
-}
-
 function wsGetAppStatus() {
-//	event.preventDefault();
 	var ab = new ArrayBuffer(3);
 	var bin = new DataView(ab);
 	
@@ -91,26 +71,9 @@ function wsGetAppStatus() {
 	websocket.send(bin.buffer);
 }
 
-function wsBinStateGet(cmd) {
-//	event.preventDefault();
-	var ab = new ArrayBuffer(3);
-	var bin = new DataView(ab);
-	
-	bin.setUint8(wsBinConst.wsCmd, wsBinConst.getCmd);
-	bin.setUint8(wsBinConst.wsSysId, 2); //AppClass.sysId = 1
-	bin.setUint8(wsBinConst.wsSubCmd, cmd);
-	
-	websocket.send(bin.buffer);
-}
-
 //Here we put some initial code which starts after DOM loaded
-//var binState;
-var binStates; 
 function onDocumentRedy() {
-	//Init
 	initWS();
-	// updateState();
-	// setInterval(updateState, 5000);
 }
 
 document.addEventListener('DOMContentLoaded', onDocumentRedy);
