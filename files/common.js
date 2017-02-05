a7dae938aee236af0e8b85b8455ba8d36220a249
Alexander V. Ribchansky <avr39ripe@gmail.com>
2017-02-05 23:58:29 +0200

    Fix BinStateHttpClass
    Remove useless _nameLength field and _updateNameLength method, use
    _name.length() instead
    
    Fix js-class BinStatesHttpClass to get names properly

--- files/common.js
+++ files/common.js
@@ -98,11 +98,14 @@ BinStateClass.prototype.wsSetState = function (state) {
 }
 
 BinStateClass.prototype.wsGotName = function (bin) {
-	var strBuffer = new Uint8Array(bin.byteLength);
-    for (var i = 0; i < strBuffer.length - (wsBinConst.wsPayLoadStart + 1); i++) {
+	var strBuffer = new Uint8Array(bin.byteLength -(wsBinConst.wsPayLoadStart + 1));
+	console.log.bind(console)(`uid = ${this.uid}, bin.byteLength = ${bin.byteLength}`);
+
+    for (var i = 0; i < strBuffer.length; i++) {
         strBuffer[i] = bin.getUint8(wsBinConst.wsPayLoadStart + 1 + i);
-//        console.log.bind(console)(`uid = ${uid}, strBuffer[${i}] = ${bin.getUint8(wsBinConst.wsPayLoadStart + 1 + i)}`);
+//        console.log.bind(console)(`uid = ${this.uid}, strBuffer[${i}] = ${bin.getUint8(wsBinConst.wsPayLoadStart + 1 + i)}`);
     }
+
     this._name = new TextDecoder().decode(strBuffer)
     this.renderName();
 }
