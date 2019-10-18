c767061b1a685b52cb7439f2c71b8d67960d7ab2
Alexander V. Ribchansky <avr39ripe@gmail.com>
2019-10-18 13:20:52 +0300

    Draft and ugly port of html/css/js stuff to support Sming 4

---
+++ files/index_new.js
@@ -0,0 +1,37 @@
+'use strict';
+
+//var appStatus
+//var binStates;
+//var tempsensors;
+//var tempsensorsHome;
+
+//import websocket from './websocket';
+import AppStatusClass from 'appStatus';
+import BinStatesClass from 'binStates';
+import { initWS, websocket, wsEnablers, wsBinProcessors } from 'websocket';
+//import TempsensorsClass from 'tempsensors';
+
+//Here we put some initial code which starts after DOM loaded
+function onDocumentRedy() {
+//	tempsensors = new TempsensorsClass('/temperature.json', 0);
+//	tempsensors.enable(true);
+//	setInterval(function () { tempsensors.wsGetAllTemperatures(); }, 5000);
+//	
+//	tempsensorsHome = new TempsensorsClass('/temperatureHome.json', 1);
+//	tempsensorsHome.enable(true);
+//	setInterval(function () { tempsensorsHome.wsGetAllTemperatures(); }, 5000);
+
+	var appStatus = new AppStatusClass();
+	var binStates = new BinStatesClass();
+	
+	wsEnablers.push(appStatus.enable.bind(appStatus));
+	wsEnablers.push(binStates.enableStates.bind(binStates));
+	wsEnablers.push(binStates.enableButtons.bind(binStates));
+	
+	wsBinProcessors[AppStatusClass.sysId] = appStatus.wsBinProcess.bind(appStatus);
+	wsBinProcessors[BinStatesClass.sysId] = binStates.wsBinProcess.bind(binStates);
+	
+	initWS();
+}
+
+document.addEventListener('DOMContentLoaded', onDocumentRedy);
