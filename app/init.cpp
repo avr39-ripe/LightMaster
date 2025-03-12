28abed7c55407c08c06d8368681b322814efe30b
Alexander V. Ribchansky <avr39-ripe@gmail.com>
2025-03-12 14:20:36 +0000

    Fix print format

--- app/init.cpp
+++ app/init.cpp
@@ -12,7 +12,7 @@ AppClass App;
 
 void init()
 {
-		Serial.printf(_F("INITIAL Free Heap: %d\n"), system_get_free_heap_size());
+		Serial.printf(_F("INITIAL Free Heap: %ld\n"), system_get_free_heap_size());
         App.init();
         App.start();
 }
